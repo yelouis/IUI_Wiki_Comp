e1da490671d6f7689d584b523066d54001843e0e
Louis Ye <44011239+yelouis@users.noreply.github.com>
2021-10-27 15:41:34 -0500

    connection to db working form python side

--- modules/wiki_db.py
+++ modules/wiki_db.py
@@ -1,4 +1,16 @@
 import psycopg2
 
-import wiki_dump as wd
+# import wiki_dump as wd
 
+try:
+    conn = psycopg2.connect("dbname='wikipedia' user='mathcsadmin' host='127.0.0.1' password='corgiPower!'")
+except:
+    print("I am unable to connect to the database")
+
+cur = conn.cursor()
+cur.execute("""INSERT INTO public."article" VALUES (1, 'test1', 'testURL1', 'testText1')""")
+conn.commit()
+cur.execute("""SELECT * FROM public.article""")
+
+rows = cur.fetchall()
+print(rows)
