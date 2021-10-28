6b1275f571e454bcde25c3974e1e571b576cd57e
Louis Ye <44011239+yelouis@users.noreply.github.com>
2021-10-28 12:42:37 -0500

    Update wiki_db.py

--- modules/wiki_db.py
+++ modules/wiki_db.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-# import wiki_dump as wd
+import wiki_dump as wd
 
 try:
     conn = psycopg2.connect("dbname='wikipedia' user='mathcsadmin' host='127.0.0.1' password='corgiPower!'")
@@ -8,8 +8,24 @@ except:
     print("I am unable to connect to the database")
 
 cur = conn.cursor()
-cur.execute("""INSERT INTO public."article" VALUES (1, 'test1', 'testURL1', 'testText1')""")
+
+wiki_dump = wd.XMLDumpParser("../dumps/simplewiki-latest-pages-meta-history.xml.bz2")
+
+amount = 11
+inserts = wiki_dump.parse_n_pages(amount)
+
+for article in inserts.values():
+    try:
+        cur.execute(f"""INSERT INTO public."article" VALUES ({article.id}, '{article.title}', {article.current_id})""")
+    except:
+        print("pageID already exists")
 conn.commit()
+
+# inserts = wiki_dump.parse_n_pages(amount)
+#
+# for article in inserts.values():
+#     cur.execute(f"""INSERT INTO public."article" VALUES ({article.id}, '{article.title}', {article.current_id})""")
+# conn.commit()
 cur.execute("""SELECT * FROM public.article""")
 
 rows = cur.fetchall()
