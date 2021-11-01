c340bd52b74a78789a7f34ed7680a3229afc206a
SpartanPip <philly.donnelly@gmail.com>
2021-11-01 16:59:02 -0500

    started the code to populate the revisions data table

--- modules/wiki_db.py
+++ modules/wiki_db.py
@@ -19,7 +19,40 @@ for article in inserts.values():
         cur.execute(f"""INSERT INTO public."article" VALUES ({article.id}, '{article.title}', {article.current_id})""")
     except:
         print("pageID already exists")
+
+    # for revision in article.revisions:
+    # 	try:
+	   #      cur.execute(f"""INSERT INTO public."revisionHistory" VALUES (
+	   #      	{revision.id}, 
+	   #      	'{revision.title}', 
+	   #      	{revision.date}, 
+	   #      	{revision.author_metrics(revision.id)},
+	   #      	{revision.num_edits}, 
+	   #      	{revision.num_edits_regis}, 
+	   #      	{revision.num_edits_anom}, 
+	   #      	{revision.internal_links(revision.id)}, 
+	   #      	{revision.external_links(revision.id)}, 
+	   #      	{len(revision.text)}, 
+	   #      	{revision.author_metrics(revision.id)}, 
+	   #      	{revision.admin_edits_prop},
+	   #      	{revision.article_age(revision.id)},
+	   #      	{article.revisions.values().index(revision)},
+	   #      	{revision.mean_revision_time},
+	   #      	{article.id}, )""")
+	   #      # info we cannot get yet:
+	   #      # title
+	   #      # num_edits
+	   #      # num_edits_regis
+	   #      # num_edits_anom
+	   #      # art_length - depending on what this is asking. Right now I am just getting text length
+	   #      # author_diversity - this comes from author_metrics(revision.id)? What is the return type of this function?
+	   #      # admin_edits_prop
+	   #      # mean_revision_time
+	   #  except:
+	   #      print("id already exists")
+
 conn.commit()
+
 
 # inserts = wiki_dump.parse_n_pages(amount)
 #
