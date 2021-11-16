273343a1b7603480f07931336ba75d076c098bdd
donnellyp <donnellyp@olin302-03.mathcs.carleton.edu>
2021-11-15 21:36:04 -0600

    lil more formatting

--- modules/webpage.py
+++ modules/webpage.py
@@ -5,6 +5,9 @@ access = db.DatabaseAccess()
 
 app = Flask(__name__)
 
+a_data = []
+r_data = []
+
 def helper(article_id):
 	print(article_id)
 
@@ -14,23 +17,24 @@ def home():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+	global a_data
+	global r_data
 	if request.form['submit_button'] == "Search Article":
 		text = request.form['article']
 		processed_text = text.upper()
 		# when we pull article by title, do API disambiguation, then if there's
 		# only 1 option, pass that as parameter to psycopg2, otherwise, pass first option?
 
-		data = access.pullArticleByID(processed_text)
-		return render_template("home.html", article_data=data)
+		a_data = access.pullArticleByID(processed_text)
+		return render_template("home.html", article_data=a_data, revision_data=r_data)
 
 	elif request.form['submit_button'] == "Search Revision":
 		text = request.form['revision']
 		processed_text = text.upper()
 		print(processed_text)
 
-		data = access.pullRevisionByID(processed_text)
-		print("data: ", data)
-		return render_template("home.html", revision_data=data)
+		r_data = access.pullRevisionByID(processed_text)
+		return render_template("home.html", article_data=a_data, revision_data=r_data)
 
 	return render_template("home.html")
 
