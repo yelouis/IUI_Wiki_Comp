95513fae9abdbef36457737cd94d88e46ace4e8b
nhayes-rich <nhayesrich@gmail.com>
2022-02-09 15:33:28 -0600

    ...

--- modules/modeling.R
+++ modules/modeling.R
@@ -3,20 +3,25 @@ library(RPostgreSQL)
 library(car)
 library(jtools)
 
+# Set up database connection
 con <- dbConnect(drv=PostgreSQL(),
-                 user="mathcsadmin",
-                 password="corgiPower!",
+                 user="...",
+                 password="...",
                  host="127.0.0.1",
                  dbname="wikipedia")
 
+# retrieve all articles and revisions
 articles <- dbGetQuery(con, "SELECT * FROM public.article;")
 revisions <- dbGetQuery(con, 'SELECT revision_id,title,num_internal_links,num_external_links,article_length,article_id,flesch,kincaid,num_images,average_sentence_length,date FROM public."revisionHistory";')
 
+# zero out missing entries
 revisions$num_images[revisions$num_images == -1] <- 0
 revisions$average_sentence_length[revisions$average_sentence_length == -1] <- 0
 revisions$num_internal_links[revisions$num_internal_links == -1] <- 0
 revisions$num_external_links[revisions$num_external_links == -1] <- 0
 
+# filter out any entries that have non-sensical flesch/kincaid scores
+# as those entries are usually spam 
 summarized_rev_filtered <- revisions %>%
   group_by(article_id) %>%
   filter(flesch > 0, flesch < 100, kincaid > -3.4, kincaid < 20) %>%
@@ -28,6 +33,7 @@ summarized_rev_filtered <- revisions %>%
             median_num_im = median(num_images, na.rm=TRUE), 
             median_asl = median(average_sentence_length, na.rm=TRUE))
 
+# combine revision properties with article-level data
 total <- articles %>% 
   left_join(summarized_rev_filtered, by = c("id" = "article_id")) %>%
   mutate(gvg = as.factor(ifelse(article_quality == 0, 0, 1)),
@@ -36,6 +42,7 @@ total <- articles %>%
 
 log_form <- gvg ~ log(num_edits) + log(num_unique_authors) + author_diversity + log(age) + log(currency) + log(median_nil + 1) + log(median_nel + 1) + log(median_len + 1) + median_flesch + median_kincaid + log(median_num_im + 1) + log(median_asl + 1) + log(author_density + 1) + log(author_score + 1)
 
+# generalized linear regression model
 total.glm <- glm(log_form, data = total, family = "binomial")
 total.glm.red1 <- update(total.glm, . ~ . - median_flesch)
 total.glm.red2 <- update(total.glm.red1, . ~ . - log(currency))
