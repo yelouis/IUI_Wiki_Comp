4fe9239ad04a723c3001b83ae93a6bfb32f2b03c
David Anderson <andersond2@carleton.edu>
2022-02-16 16:27:37 -0600

    beginning of docwordmtx

--- modules/nlp/stopwords.py
+++ modules/nlp/stopwords.py
@@ -2,14 +2,20 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
-stop_words = set(stopwords.words('english'))
+#word_tokens = word_tokenize(example_sent)
 
-word_tokens = word_tokenize(example_sent)
+# filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
 
-filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+# filtered_sentence = []
 
-filtered_sentence = []
+# for w in word_tokens:
+#     if w not in stop_words:
+#         filtered_sentence.append(w)
 
-for w in word_tokens:
-    if w not in stop_words:
-        filtered_sentence.append(w)
+def remove_stopwords(text):
+    filtered = []
+    stop_words = set(stopwords.words('english'))
+    for word in text:
+        lower_word = word.lower()
+        if lower_word not in stop_words:
+            filtered.append(lower_word)
